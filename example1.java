c3a32247e8dc29f795e0ce0158d54f6f566f30a4
Prajwal <prajwal9625@gmail.com>
2026-02-28 21:59:55 +0530

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class example1
+	 {
+		public static void main(String[] args)
+		{
+			for(int i=1;i<=11;i++)
+			{
+				System.out.println("The output is:"+i);
+			}
+		}
+	}
