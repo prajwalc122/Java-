024f2ea5fe8479b8d6de89c53dbab81eb070925d
Prajwal <prajwal9625@gmail.com>
2026-02-28 21:58:47 +0530

    Add files via upload

---
+++ poor.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class poor
+		{
+			public static void main(String[] args)
+			{
+					 int number=11;
+					 number=32;
+					 System.out.println("The Output will be "+number);
+			}
+		}
