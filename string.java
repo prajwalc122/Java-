96e97e81e2d4d09db1e4ba603dd263004de312f1
Prajwal <prajwal9625@gmail.com>
2026-03-01 20:34:09 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class string{
+  public static void main(String[] args) {
+    String firstName = "prajwal ";
+    String lastName = "c";
+    String fullName = firstName + lastName;
+    System.out.println("The Full name is :"+fullName);  
+  }
+}
