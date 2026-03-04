fb9f407c376aedd52c563888764f717261025e81
Prajwal <prajwal9625@gmail.com>
2026-03-04 16:41:00 +0530

    Add files via upload

---
+++ moment.java
@@ -0,0 +1,22 @@
+import java.io.*;
+public class moment
+{
+	public static void main(String[] args)
+	{
+		//student data 
+		String studentname="prajwal c";
+		int studentage=18;
+		int studentpercent=88;
+		double studentfees=233.43;
+		char studentgrade='B';
+		
+		//print the variables
+		
+		System.out.println("Student name is :"+studentname);
+		System.out.println("Student age is :"+studentage);
+		System.out.println("Student percent is :"+studentpercent);
+		System.out.println("Student fees is :"+studentfees);
+		System.out.println("Student Grade is :"+studentgrade);
+		}
+}
+	
