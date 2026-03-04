7aacd9b07e2138bc50b46debc06a09f32e38cbbb
Prajwal <prajwal9625@gmail.com>
2026-03-04 16:40:28 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class area
+{
+	public static void main(String[] args)
+	{
+		int length,width,area;
+		length=4;
+		width=8;
+		//calucate the area 
+		area=width*length;
+		//print the variables
+		System.out.println("Length is :"+length);
+		System.out.println("Width is :"+width);
+		System.out.println("Area of Rectangle :"+area);
+	}
+}
+		
