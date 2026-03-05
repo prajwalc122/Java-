8ff00c191597c2f060006143613db28e91cf7586
Prajwal <prajwal9625@gmail.com>
2026-03-05 21:48:30 +0530

    Add files via upload

---
+++ data.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class data{
+	public static void main(String[] args)
+	{
+		 float f1 = 35e3f;
+		double d1 = 12d;
+		System.out.println(f1);
+		System.out.println(d1);
+	}
+}
+
+/*
+Key Points
+
+Size: 4 bytes (32 bits)
+
+Precision: about 6–7 decimal digits
+
+Must end with f or F in Java.
+*/
