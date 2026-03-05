86aae0ab701f65edcaf49104905dd0d01e849035
Prajwal <prajwal9625@gmail.com>
2026-03-05 21:55:44 +0530

    Add files via upload

---
+++ inte.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class inte
+{
+public static void main(String[] args)
+{
+	int age = 18;
+	int marks = 95;
+	int year = 2026;
+
+	System.out.println(age);
+	System.out.println(marks+"marks in="+year);
+}
+}
