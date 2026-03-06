78db6e4ebbb1b89bf7c69660379e84f59281d81a
Prajwal <prajwal9625@gmail.com>
2026-03-06 21:38:23 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class odd
+{
+    public static void main(String[] args)
+    {
+        int num = 7;
+
+        if(num % 2 == 0)
+        {
+            System.out.println("Even Number");
+        }
+        else
+        {
+            System.out.println("Odd Number");
+        }
+    }
+}
