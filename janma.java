81bc40dacd6cdf5bcd12a81fc42430da33fae6bc
Prajwal <prajwal9625@gmail.com>
2026-03-07 17:11:27 +0530

    Add files via upload

---
+++ janma.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class janma
+{
+	public static void main(String[] args)
+	{
+	
+		 long num=494900009L;
+		 System.out.println("Its the whole number and long datatypes represent in:"+num);
+	}
+}
