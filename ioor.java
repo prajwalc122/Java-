0763eebfd894b2d399c0b1bf12f64c01fb0e2ae3
Prajwal <prajwal9625@gmail.com>
2026-03-07 17:31:44 +0530

    Add files via upload

---
+++ ioor.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class ioor
+{
+	public static void main(String[] args)
+	{
+		float f1 = 35e3f;
+		double d1 = 12E4d;
+		System.out.println(f1);
+		System.out.println(d1);
+	}
+}
