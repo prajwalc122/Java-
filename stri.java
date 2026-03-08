935e7d830fb2100a7c02374292c27cd2dfa908e7
Prajwal <prajwal9625@gmail.com>
2026-03-08 22:14:29 +0530

    Add files via upload

---
+++ stri.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class stri
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String name;
+		System.out.println("Enter your name:");
+		name= sc.nextLine();
+		
+		System.out.println("You entered name is :"+name);
+	}
+}
