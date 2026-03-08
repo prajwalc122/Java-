a3003feb0e06528f233f3f9d13a9e5a8d6ab2043
Prajwal <prajwal9625@gmail.com>
2026-03-08 22:13:19 +0530

    Add files via upload

---
+++ dobhai.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class dobhai
+{
+	public static void main(String[] args)
+	{
+		Scanner sc =new Scanner(System.in);
+		double DOUB;
+		System.out.println("Enter the double numbers please:");
+		DOUB=sc.nextDouble();
+		
+		System.out.println("THE DOUBLE VALUE IS :"+DOUB);
+	}
+}
