8741227425ec4a8acb92e84d37d3529e50c5d87e
Prajwal <prajwal9625@gmail.com>
2026-03-09 17:36:07 +0530

    Add files via upload

---
+++ another.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class another
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String nickname;
+		System.out.println("Enter yourss nickname:");
+		nickname=sc.nextLine();
+		System.out.println("The actual nickname is :"+nickname);
+	}
+}
