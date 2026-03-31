9532d7e138577ad6ddab4e842e5501801725e841
Prajwal <prajwal9625@gmail.com>
2026-03-31 20:50:54 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class Even{
+    public static void main(String[] args) {
+        int num = 9;
+
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
