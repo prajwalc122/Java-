ac0b133d03a598b787a78fb5ea77a3a2aefdca0a
Prajwal <prajwal9625@gmail.com>
2026-04-04 22:08:19 +0530

    Add files via upload

---
+++ newl.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.io.*;
+public class newl{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your name :"+a);
+int a =sc.nextInt();
+}
+}
