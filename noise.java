438d0f2f7e81342b0115a448258d86e4baf295e7
Prajwal <prajwal9625@gmail.com>
2026-04-16 21:03:45 +0530

    Add files via upload

---
+++ noise.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class parent{
+	void show(){
+			System.out.println("this is parent class");
+		}
+	}
+class child extends parent {
+		void display(){
+		System.out.println("This is parent class");
+	}
+}
+
+public class noise
+{
+
+	public static void main (String[] args)
+	{
+	 `	child=new child
