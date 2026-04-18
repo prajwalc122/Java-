1676f085ef179ee71d36033ca158a8bca4880540
Prajwal <prajwal9625@gmail.com>
2026-04-18 22:16:28 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Test {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
