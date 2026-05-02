bfdb2dc9bb57a4b59923580ab859879c8597263c
Prajwal <prajwal9625@gmail.com>
2026-05-02 09:13:50 +0530

    Add files via upload

---
+++ Bird.java
@@ -0,0 +1,16 @@
+
+class Bird 
+{
+
+	{
+    String name;
+
+    void fly();{
+        System.out.println("Bird is flying");
+    }
+
+    void layEggs;() {
+        System.out.println("Bird lays eggs");
+    }
+}
+}
