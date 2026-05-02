d497620e77d025cb25be50ec9e3a0cbc858416c9
Prajwal <prajwal9625@gmail.com>
2026-05-02 09:07:13 +0530

    Add files via upload

---
+++ classes.java
@@ -0,0 +1,11 @@
+public class classes{
+    public static void main(String[] args) {
+
+        Student s1 = new Student(); // object
+
+        s1.name = "Prajwal";
+        s1.age = 20;
+
+        s1.display();
+    }
+}
