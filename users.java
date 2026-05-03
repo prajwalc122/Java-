c42a82c2f0087aca6f70ebee45525fcbf3f48cb6
Prajwal <prajwal9625@gmail.com>
2026-05-03 13:19:34 +0530

    Add files via upload

---
+++ users.java
@@ -0,0 +1,17 @@
+public class users 
+{
+	void add(String a, String b) {
+	System.out.println("The String value is :"+a+b);
+	}
+	void age(int a){
+	System.out.println("Is Age is :"+a);
+	}
+	public static void main(String[] args){
+	users news = new users();
+	
+	
+	news.add("prajwal","c");
+	news.age(18);
+	}
+}
+	
