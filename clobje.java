f464db4d585fffe552ed882cd035588d911e98d8
Prajwal <prajwal9625@gmail.com>
2026-05-05 06:24:50 +0530

    Add files via upload

---
+++ clobje.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class student{
+	int age;
+	String name;
+	
+	void himself(){
+		System.out.println("He was a Boy ");
+		}
+	}
+	
+	public class clobje
+	{
+		public static void main(String[] args)
+		{
+			student s1=new student();
+			s1.age=18;
+			s1.name="prajwal c";
+			s1.himself();
+			System.out.println(s1.name);
+			System.out.println(s1.age);
+		}
+	}
