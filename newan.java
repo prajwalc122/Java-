b21a2550cf6bae4aad69dffc2d6b49b90fc97786
Prajwal <prajwal9625@gmail.com>
2026-05-06 22:16:07 +0530

    Add files via upload

---
+++ newan.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class labours
+{	
+	String name;
+	int salary;
+	int age;
+	
+	void read()
+	{
+	System.out.println("His Detailes is Shown in Below:");
+	}
+}
+public class newan
+{
+	public static void main(String[] args)
+	{
+		labours l1= new labours();
+		l1.name="yograja";
+		l1.salary=22000;
+		l1.age=34;
+		l1.read();
+	}
+}
