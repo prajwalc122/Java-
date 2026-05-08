f98c78c46b77fc7b7b554dc971b427b5b43f3008
Prajwal <prajwal9625@gmail.com>
2026-05-08 21:37:04 +0530

    Add files via upload

---
+++ arrange.java
@@ -0,0 +1,18 @@
+class Parent {
+    Parent() {
+        System.out.println("Parent Constructor");
+    }
+}
+
+class Child extends Parent {
+    Child() {
+        System.out.println("Child Constructor");
+    }
+}
+
+public class arrange{
+    public static void main(String[] args) {
+
+        Child c = new Child();
+    }
+}
