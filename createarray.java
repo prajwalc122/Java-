c276af9dfd68f6fa46e2c207892d4b14cd3040d2
Prajwal <prajwal9625@gmail.com>
2026-05-13 22:46:59 +0530

    Add files via upload

---
+++ createarray.java
@@ -0,0 +1,15 @@
+class createarray
+{
+    public static void main(String args[])
+    {
+        // Step 1: Create array
+        int arr[] = {10,20,30,40,50};
+
+        // Step 2: Print elements
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+    }
+}
