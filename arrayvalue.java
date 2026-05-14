f336278b86e0e8ed6dfe23d58fcf80d2a2a8fc46
Prajwal <prajwal9625@gmail.com>
2026-05-14 18:48:09 +0530

    Add files via upload

---
+++ arrayvalue.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class arrayvalue
+{
+	public static void main(String[] args)
+	{
+		String arr[]={"prajwal","ninja","vinod"};
+		
+		System.out.println("The String value is :"+arr[1]+","+arr[2]+","+arr[0]);
+		System.out.println("The program is Finishes");
+	}
+}
