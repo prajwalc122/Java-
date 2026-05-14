5e48fbbf3cd6261adf9c6bbd9f80b85d09991cd1
Prajwal <prajwal9625@gmail.com>
2026-05-14 18:31:09 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,9 @@
+class array
+{
+	public static void main(String[] args)
+	{
+		int arr[]={11,44,65,24};
+		// this is an array program and i learn't it.
+		System.out.println("The first index value is : "+arr[0]);
+	}
+}
