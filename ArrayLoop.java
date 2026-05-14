be18d53da85d03c9b12e85683f5de2fa317c5979
Prajwal <prajwal9625@gmail.com>
2026-05-14 18:37:02 +0530

    Add files via upload

---
+++ ArrayLoop.java
@@ -0,0 +1,17 @@
+class ArrayLoop
+{
+    public static void main(String args[])
+    {
+        int array[] = {10,20,30,40,50,60,70,80,90,100,10001,3131,3444,5566,6544};
+
+        // Loop starts from index 0
+        for(int i = 0; i < array.length; i++)
+        {
+            System.out.println(array[i]);
+			
+    
+    }
+	System.out.println("The program Was Finishes.");
+    }
+	
+}
