6cc6c7567636f2ae5c37e7f0c9e55bc1a8878b2e
Prajwal <prajwal9625@gmail.com>
2026-05-15 15:14:01 +0530

    Add files via upload

---
+++ appendinusingstringbuffers.java
@@ -0,0 +1,11 @@
+public class appendinusingstringbuffers
+{
+	public static void main(String[] args)
+	{
+		StringBuffer p=new StringBuffer("Hello");
+		p.append(" world");
+		
+		System.out.println("The values is:"+p);
+	}
+}
+		
