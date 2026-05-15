cbc5f716fdbba86a5e1caae1b00441326375e6ab
Prajwal <prajwal9625@gmail.com>
2026-05-15 15:41:08 +0530

    Add files via upload

---
+++ stringbufferinsert.java
@@ -0,0 +1,9 @@
+public class stringbufferinsert
+{
+	public static void main(String[] args)
+	{
+		StringBuffer p1=new StringBuffer("kuvempu");
+		p1.insert(7," university");//insert the university on 7 th index
+		System.out.println(p1);
+	}
+}
