ef388b01fab681837565c03087d3226579b559fa
Prajwal <prajwal9625@gmail.com>
2026-05-16 17:25:32 +0530

    Add files via upload

---
+++ DEmoappend.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class DEmoappend
+{
+	public static void main(String[] args)
+	{
+		StringBuffer sb=new StringBuffer("im prepare for:");
+		sb.append("kannada");
+		
+		System.out.println(sb);
+	}
+}
