5d908e0a122e1db40bb8bad42339056b430aabf9
Prajwal <prajwal9625@gmail.com>
2026-05-17 12:50:46 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,17 @@
+import java.io.*;	
+class Example{
+
+    private int a = 1;
+    int b = 2;              // default
+    protected int c = 3;
+    public int d = 4;
+	public static void main(String[] args)
+	{
+    void display() {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+	}
+}
