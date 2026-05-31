7bebf51770456520a524f15dbcd396ddeb2a2a87
Prajwal <prajwal9625@gmail.com>
2026-05-31 23:06:05 +0530

    Add files via upload

--- classes.java
+++ classes.java
@@ -1,11 +1,16 @@
-public class classes{
+class Student{        //class is a collection of objects
+    int id = 101;
+    String name = "Prajwal";
+
+    void display() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+    }
+}
+
+public class Classes{
     public static void main(String[] args) {
-
-        Student s1 = new Student(); // object
-
-        s1.name = "Prajwal";
-        s1.age = 20;
-
+        Student s1 = new Student(); // Object creation
         s1.display();
     }
 }
